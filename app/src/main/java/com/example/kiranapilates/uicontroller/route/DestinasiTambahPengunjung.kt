f8e7166a4f12fd48d4a7ded671cba58d7853c7e1
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 01:49:09 +0700

    membuat route untuk tambah pengunjung

---
+++ app/src/main/java/com/example/kiranapilates/uicontroller/route/DestinasiTambahPengunjung.kt
@@ -0,0 +1,6 @@
+package com.example.kiranapilates.uicontroller.route
+
+object DestinasiTambahPengunjung : DestinasiNavigasi {
+    override val route = "tambah_pengunjung"
+    override val titleRes = "Tambah Pengunjung"
+}
