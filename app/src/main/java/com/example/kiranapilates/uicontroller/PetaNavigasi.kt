72690a228180e7079a17dbe97e4d2ce2a78e89bf
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 02:14:49 +0700

    menambahkan composable detail pengunjung dalam PetaNavigasi.kt

--- app/src/main/java/com/example/kiranapilates/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/kiranapilates/uicontroller/PetaNavigasi.kt
@@ -2,12 +2,16 @@ package com.example.kiranapilates.uicontroller
 
 import androidx.compose.runtime.Composable
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.kiranapilates.uicontroller.route.DestinasiDaftarPengunjung
+import com.example.kiranapilates.uicontroller.route.DestinasiDetailPengunjung
 import com.example.kiranapilates.uicontroller.route.DestinasiTambahPengunjung
 import com.example.kiranapilates.view.HalamanDaftarPengunjung
+import com.example.kiranapilates.view.HalamanDetailPengunjung
 import com.example.kiranapilates.view.HalamanTambahPengunjung
 import com.example.kiranapilates.viewmodel.provider.PenyediaViewModel
 
@@ -32,6 +36,15 @@ fun PetaNavigasi() {
         }
         composable(route = DestinasiTambahPengunjung.route) {
             HalamanTambahPengunjung(
+                viewModel = viewModel(factory = PenyediaViewModel.Factory),
+                onNavigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(
+            route = DestinasiDetailPengunjung.idWithArgs,
+            arguments = listOf(navArgument("id_pengunjung") { type = NavType.StringType })
+        ) {
+            HalamanDetailPengunjung(
                 viewModel = viewModel(factory = PenyediaViewModel.Factory),
                 onNavigateBack = { navController.popBackStack() }
             )
