ac9d25f292aeb316cb80203ee37a9c31e0104ece
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 01:12:31 +0700

    tambah PenyediaViewModel untuk masukkan repository ke ViewModel

---
+++ app/src/main/java/com/example/kiranapilates/viewmodel/provider/PenyediaViewModel.kt
@@ -0,0 +1,21 @@
+package com.example.kiranapilates.viewmodel.provider
+
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.kiranapilates.KiranaPilatesApp
+import com.example.kiranapilates.viewmodel.DaftarPengunjungViewModel
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            // Memanggil repository dari container yang ada di KiranaPilatesApp
+            DaftarPengunjungViewModel(kiranaPilatesApp().container.pengunjungRepository)
+        }
+    }
+}
+
+fun CreationExtras.kiranaPilatesApp(): KiranaPilatesApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as KiranaPilatesApp)
