ffb5585a2d35693707ab1b4b2fcff860884d75b1
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 00:02:50 +0700

    tambah service API untuk tambah/create pengunjung

--- app/src/main/java/com/example/kiranapilates/apiservice/KiranaApiService.kt
+++ app/src/main/java/com/example/kiranapilates/apiservice/KiranaApiService.kt
@@ -19,4 +19,20 @@ interface KiranaApiService {
         @Header("Authorization") token: String
     ): BasicResponse
 
+
+    // --- 2. PENGUNJUNG (Halaman 3 - 6) ---
+    @GET("pengunjung/read.php")
+    suspend fun getAllPengunjung(
+        @Header("Authorization") token: String
+    ): PengunjungResponse
+
+    @FormUrlEncoded
+    @POST("pengunjung/create.php")
+    suspend fun insertPengunjung(
+        @Header("Authorization") token: String,
+        @Field("nama_lengkap") nama: String,
+        @Field("no_hp") hp: String,
+        @Field("tipe_pengunjung") tipe: String
+    ): BasicResponse
+
 }
