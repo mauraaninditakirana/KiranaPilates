656e2ce8219c0b2863ad5a6e6e06ccd5dfb12f4a
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 02:29:06 +0700

    daftarkan route edit pengunjung di petanavigasi

--- app/src/main/java/com/example/kiranapilates/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/kiranapilates/uicontroller/PetaNavigasi.kt
@@ -9,9 +9,11 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.kiranapilates.uicontroller.route.DestinasiDaftarPengunjung
 import com.example.kiranapilates.uicontroller.route.DestinasiDetailPengunjung
+import com.example.kiranapilates.uicontroller.route.DestinasiEditPengunjung
 import com.example.kiranapilates.uicontroller.route.DestinasiTambahPengunjung
 import com.example.kiranapilates.view.HalamanDaftarPengunjung
 import com.example.kiranapilates.view.HalamanDetailPengunjung
+import com.example.kiranapilates.view.HalamanEditPengunjung
 import com.example.kiranapilates.view.HalamanTambahPengunjung
 import com.example.kiranapilates.viewmodel.provider.PenyediaViewModel
 
@@ -45,6 +47,15 @@ fun PetaNavigasi() {
             arguments = listOf(navArgument("id_pengunjung") { type = NavType.StringType })
         ) {
             HalamanDetailPengunjung(
+                viewModel = viewModel(factory = PenyediaViewModel.Factory),
+                onNavigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(
+            route = DestinasiEditPengunjung.idWithArgs,
+            arguments = listOf(navArgument("id_pengunjung") { type = NavType.StringType })
+        ) {
+            HalamanEditPengunjung(
                 viewModel = viewModel(factory = PenyediaViewModel.Factory),
                 onNavigateBack = { navController.popBackStack() }
             )
