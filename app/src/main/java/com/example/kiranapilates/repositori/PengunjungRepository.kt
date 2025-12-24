b837fa6c5ad080be22204910667f5b4e7da3495a
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 01:45:31 +0700

    tambah fungsi insertPengunjung di PengunjungRepository

--- app/src/main/java/com/example/kiranapilates/repositori/PengunjungRepository.kt
+++ app/src/main/java/com/example/kiranapilates/repositori/PengunjungRepository.kt
@@ -1,13 +1,27 @@
 package com.example.kiranapilates.repositori
 
 import com.example.kiranapilates.apiservice.KiranaApiService
+import com.example.kiranapilates.modeldata.Pengunjung
+import com.example.kiranapilates.modeldata.BasicResponse
 import com.example.kiranapilates.modeldata.PengunjungResponse
 
 interface PengunjungRepository {
     suspend fun getPengunjung(token: String): PengunjungResponse
+
+    suspend fun insertPengunjung(token: String, pengunjung: Pengunjung): BasicResponse
 }
 
-class NetworkPengunjungRepository(private val kiranaApiService: KiranaApiService) : PengunjungRepository {
+    class NetworkPengunjungRepository(private val kiranaApiService: KiranaApiService) : PengunjungRepository {
     override suspend fun getPengunjung(token: String): PengunjungResponse =
         kiranaApiService.getAllPengunjung(token)
+
+        override suspend fun insertPengunjung(token: String, pengunjung: Pengunjung): BasicResponse {
+            return kiranaApiService.insertPengunjung(
+                token,
+                pengunjung.nama_lengkap,
+                pengunjung.no_hp,
+                pengunjung.tipe_pengunjung
+            )
+        }
+
 }
