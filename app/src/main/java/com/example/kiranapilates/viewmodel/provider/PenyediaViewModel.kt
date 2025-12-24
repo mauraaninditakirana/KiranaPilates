f22e827b40dbb4f89d7bba1064e0e987a5f19c56
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 01:53:25 +0700

    menambahkan initializer tambahpengunjung

--- app/src/main/java/com/example/kiranapilates/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/kiranapilates/viewmodel/provider/PenyediaViewModel.kt
@@ -7,12 +7,16 @@ import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.kiranapilates.repositori.KiranaPilatesApp
 import com.example.kiranapilates.viewmodel.DaftarPengunjungViewModel
+import com.example.kiranapilates.viewmodel.TambahPengunjungViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
             // Memanggil repository dari container yang ada di KiranaPilatesApp
             DaftarPengunjungViewModel(kiranaPilatesApp().container.pengunjungRepository)
+        }
+        initializer {
+            TambahPengunjungViewModel(kiranaPilatesApp().container.pengunjungRepository)
         }
     }
 }
