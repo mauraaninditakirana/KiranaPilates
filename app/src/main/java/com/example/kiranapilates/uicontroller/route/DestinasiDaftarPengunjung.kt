8c6e0103380c40f8d34ffa7f3fb646096fbb9fe6
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-25 00:58:28 +0700

    tambah route untuk DaftarPengunjung

--- app/src/main/java/com/example/kiranapilates/uicontroller/route/DestinasiDaftarPengunjung.kt
+++ app/src/main/java/com/example/kiranapilates/uicontroller/route/DestinasiDaftarPengunjung.kt
@@ -1,2 +1,6 @@
 package com.example.kiranapilates.uicontroller.route
 
+object DestinasiDaftarPengunjung : DestinasiNavigasi {
+    override val route = "daftar_pengunjung"
+    override val titleRes = "Daftar Pengunjung"
+}
