981235dd53c50e8de287b9791e429fdc65bdfdc4
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-24 23:45:45 +0700

    tambah model data Sesi

---
+++ app/src/main/java/com/example/kiranapilates/modeldata/Sesi.kt
@@ -0,0 +1,19 @@
+package com.example.kiranapilates.modeldata
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Pengunjung(
+    val id_pengunjung: String,
+    val nama_lengkap: String,
+    val no_hp: String,
+    val tipe_pengunjung: String,
+    val kuota_sisa: String,
+    val total_kunjungan: String
+)
+
+@Serializable
+data class PengunjungResponse(
+    val status: String,
+    val data: List<Pengunjung>
+)
