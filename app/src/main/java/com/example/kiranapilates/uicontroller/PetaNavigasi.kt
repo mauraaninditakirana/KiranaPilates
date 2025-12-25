d412d328e5c25107a6ebcd65d9455fbe8882f6d8
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-26 00:45:23 +0700

    mendaftarkan halamansesiupdate ke PetaNavigasi.kt

--- app/src/main/java/com/example/kiranapilates/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/kiranapilates/uicontroller/PetaNavigasi.kt
@@ -10,10 +10,14 @@ import androidx.navigation.navArgument
 import com.example.kiranapilates.uicontroller.route.DestinasiDaftarPengunjung
 import com.example.kiranapilates.uicontroller.route.DestinasiDetailPengunjung
 import com.example.kiranapilates.uicontroller.route.DestinasiEditPengunjung
+import com.example.kiranapilates.uicontroller.route.DestinasiSesi
+import com.example.kiranapilates.uicontroller.route.DestinasiSesiUpdate
 import com.example.kiranapilates.uicontroller.route.DestinasiTambahPengunjung
 import com.example.kiranapilates.view.HalamanDaftarPengunjung
 import com.example.kiranapilates.view.HalamanDetailPengunjung
 import com.example.kiranapilates.view.HalamanEditPengunjung
+import com.example.kiranapilates.view.HalamanSesi
+import com.example.kiranapilates.view.HalamanSesiUpdate
 import com.example.kiranapilates.view.HalamanTambahPengunjung
 import com.example.kiranapilates.viewmodel.provider.PenyediaViewModel
 
@@ -56,6 +60,22 @@ fun PetaNavigasi() {
             arguments = listOf(navArgument("id_pengunjung") { type = NavType.StringType })
         ) {
             HalamanEditPengunjung(
+                viewModel = viewModel(factory = PenyediaViewModel.Factory),
+                onNavigateBack = { navController.popBackStack() }
+            )
+        }
+        composable(route = DestinasiSesi.route) {
+            HalamanSesi(
+                viewModel = viewModel(factory = PenyediaViewModel.Factory),
+                onEditClick = { id -> navController.navigate("${DestinasiSesiUpdate.route}/$id") },
+                onBack = { navController.popBackStack() }
+            )
+        }
+        composable(
+            route = DestinasiSesiUpdate.idWithArgs,
+            arguments = listOf(navArgument("id_sesi") { type = NavType.StringType })
+        ) {
+            HalamanSesiUpdate(
                 viewModel = viewModel(factory = PenyediaViewModel.Factory),
                 onNavigateBack = { navController.popBackStack() }
             )
