d2b1882da17fd765cc4028cb212c16049569e3f4
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-26 00:40:23 +0700

    membuat view untuk halaman updatesesi

---
+++ app/src/main/java/com/example/kiranapilates/view/HalamanSesiUpdate.kt
@@ -0,0 +1,15 @@
+package com.example.kiranapilates.view
+
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import com.example.kiranapilates.viewmodel.EditSesiViewModel
+
+@Composable
+fun HalamanSesiUpdate(
+    viewModel: EditSesiViewModel,
+    onNavigateBack: () -> Unit
+) {
+
+    Text("Halaman Edit Sesi (Halaman 8)")
+}
