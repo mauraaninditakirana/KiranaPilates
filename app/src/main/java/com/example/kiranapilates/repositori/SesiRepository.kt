d8f8915efe373c648937dbf862616bcc45f6177b
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-26 00:26:47 +0700

    mengedit SesiRepository.kt

--- app/src/main/java/com/example/kiranapilates/repositori/SesiRepository.kt
+++ app/src/main/java/com/example/kiranapilates/repositori/SesiRepository.kt
@@ -1,13 +1,16 @@
 package com.example.kiranapilates.repositori
-
 import com.example.kiranapilates.apiservice.KiranaApiService
+import com.example.kiranapilates.modeldata.BasicResponse
 import com.example.kiranapilates.modeldata.SesiResponse
 
 interface SesiRepository {
     suspend fun getSesi(token: String): SesiResponse
+    suspend fun updateSesi(token: String, id: String, jam: String, instruktur: String): BasicResponse
 }
 
 class NetworkSesiRepository(private val kiranaApiService: KiranaApiService) : SesiRepository {
-    override suspend fun getSesi(token: String): SesiResponse =
-        kiranaApiService.getAllSesi(token)
+    override suspend fun getSesi(token: String): SesiResponse = kiranaApiService.getAllSesi(token)
+
+    override suspend fun updateSesi(token: String, id: String, jam: String, instruktur: String): BasicResponse =
+        kiranaApiService.updateSesi(token, id, jam, instruktur)
 }
