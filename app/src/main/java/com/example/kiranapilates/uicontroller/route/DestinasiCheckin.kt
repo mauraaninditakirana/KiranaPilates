f8cda802a61724c53c88483fa9bd0ed47df1166f
mauraaninditakirana <kiranamaura351@gmail.com>
2025-12-26 01:02:31 +0700

    membuat route untuk checknin

---
+++ app/src/main/java/com/example/kiranapilates/uicontroller/route/DestinasiCheckin.kt
@@ -0,0 +1,7 @@
+package com.example.kiranapilates.uicontroller.route
+
+
+object DestinasiCheckin : DestinasiNavigasi {
+    override val route = "tambah_checkin"
+    override val titleRes = "Check-in"
+}
